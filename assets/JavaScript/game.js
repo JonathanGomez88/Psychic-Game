af8fc9c7ca9789d5d5b8b26ce69e0144e760e05c
Jongomez <jongomez@comcast.net>
2018-06-04 22:55:49 -0400

    final draft

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -8,28 +8,31 @@ var guessLetters = [];
 var guessesLeft = 9;
 
 
+var compPick = letters[Math.floor(Math.random() * letters.length)];
 
 // User picks a letter
 document.onkeyup = function (event) {
     var userPick = event.key;
     // Computer picks a letter
-    var compPick = letters[Math.floor(Math.random() * letters.length)];
+   
     // If statements determine what happens next
     if (userPick === compPick) {
         ++wins;
         guessesLeft = 9;
         guessLetters = [];
+        compPick = letters[Math.floor(Math.random() * letters.length)];
         document.querySelector('#win').innerHTML = "Wins: " + wins;
     }
     else if (userPick !== compPick) {
         --guessesLeft;
         guessLetters.push(userPick);
-        document.querySelector('#userPick').innerHTML = "You've tried " + guessLetters;
+        document.querySelector('#userPick').innerHTML = "You've tried: " + guessLetters;
         document.querySelector('#guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
     }
     if (guessesLeft === 0) {
         guessesLeft = 9;
         ++losses;
+        compPick = letters[Math.floor(Math.random() * letters.length)];
         guessLetters = [];
         document.querySelector('#loss').innerHTML = "Losses: " + losses;
     }
